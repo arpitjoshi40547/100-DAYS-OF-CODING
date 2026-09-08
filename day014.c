a45395049ec89c5ecaac570b8187166920634f7d
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-08 13:45:32 +0530

    Fourteenth

---
+++ day014.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char word;
+    printf("Enter a alphabet\n");
+    scanf("%c",&word);
+    word= tolower(word);
+    if(isalpha(word))
+    {
+        if(word=='a'||word=='e'||word=='i'||word=='o'||word=='u')
+        {
+            printf("The alphabet is vowel ");
+        }
+        else
+        {
+            printf("The alphabet is a consonent");
+        }
+    }
+    else 
+    printf("The character entered is not an alphabet");
+}
