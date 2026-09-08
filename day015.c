05e183282b62275e55b4cbeef0d2292293ebdb35
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-08 15:44:05 +0530

    Fifteenth

---
+++ day015.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char word;
+    printf("Enter a character \n ");
+    scanf("%c",&word);
+    if(word>=64&&word<=96 )
+    {
+        printf("The character is upper case");
+    }
+    else if(word>=97&&word<=122)
+    {
+        printf("The character is lower case");
+    }
+    else if(word>=48&&word<=57 )
+    {
+        printf("The character is a number ");
+    }
+    else
+    {
+        printf("The character is a special character");
+    }
+    return 0;
+}
