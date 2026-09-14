5c50932a36b2700dc7d77a864e4fd441c1fc7764
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-14 11:31:43 +0530

    Correction

--- day010Q01.c
+++ day010Q01.c
@@ -9,7 +9,7 @@ int main()
     scanf("%d %d %d",&side1,&side2,&side3);
     if((side1+side2)>side3&&(side1+side3)>side2&&(side2+side3)>side1)
     {
-        if(side1==side2==side3)
+        if(side1==side2 && side2==side3)
         {
             printf("Equilateral traingle");
         }
