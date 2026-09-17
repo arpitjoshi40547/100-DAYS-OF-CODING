b25dbabeeb7cf49ebf8290f0135c693b45cb7b2a
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-17 20:51:36 +0530

    Pattern 48

--- day024Q02.c
+++ day024Q02.c
@@ -7,9 +7,9 @@
 #include <stdio.h>
 int main()
 {
-    for(int i =0 ;i<5;i++)
+    for(int i =1 ;i<=5;i++)
     {
-        for(int j =0;j<=i;j++)
+        for(int j =1;j<=i;j++)
         {
             printf("%d",j);
         }
