73a90876394da0e2800a59c6db431767fe2f5f9f
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-17 20:48:28 +0530

    Pattern 48

---
+++ day024Q02.c
@@ -0,0 +1,19 @@
+/*Write a program to print the following pattern:
+1
+12
+123
+1234
+12345 */
+#include <stdio.h>
+int main()
+{
+    for(int i =0 ;i<5;i++)
+    {
+        for(int j =0;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
