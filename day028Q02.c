6b80a9cf02c89dc94358d2da64e4fbd7695523e5
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-19 18:29:46 +0530

    To make a array and take input from user

---
+++ day028Q02.c
@@ -0,0 +1,17 @@
+// Read and print elements of a one-dimensional array.
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    int arr[n];
+    printf("Enter how many number need to be in the array\n");
+    scanf("%d", &n);
+    printf("Enter the numbers of the array\n");
+    for (int i = 0; i <= n ; i++)
+          scanf("%d", &arr[i]);
+    printf("Array elements:");
+    for (int j = 0; j <= n; j++)
+        printf("%d,", arr[j]);
+    return 0;
+}
