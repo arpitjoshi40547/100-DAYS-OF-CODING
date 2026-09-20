825ea92d308ddd26abfd51d149befd4184972766
arpitjoshi40547 <arpit.40547@stu.upes.ac.in>
2026-09-20 11:28:50 +0530

    Too count positive negative and zero elements in an array

--- day030Q02.c
+++ day030Q02.c
@@ -5,27 +5,27 @@
 int main()
 {
     int n;
-    int arr[n];
     printf("Enter how many elements u want in ur array\n");
-    scanf("%d", &n);
+    scanf("%d",&n);
     printf("Enter n no of elements in the array\n");
-    int n=0;
+    int arr[n];
+    int ne=0;
     int p=0;int z=0;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d",&arr[i]);
     }
-    for (int j = 1; j <= n; j++)
+    for (int j = 0; j < n; j++)
     {
         if (arr[j] > 0)
             p++;
-        else(arr[j]<0)
-            n++;
+        else if(arr[j]<0)
+            ne++;
         else
-            z++
+            z++;
     }
     printf("No of positive numbers in the array are:%d\n",p);
-    printf("No of negative numbers in the array are:%d\n",n);
+    printf("No of negative numbers in the array are:%d\n",ne);
     printf("No of zeros in the array are:%d\n",z);
     return 0;
 }
